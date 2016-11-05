fe1c224c19cef7ef4679570158e0b329d0c0b013
michael_xu <michael_xu@caifujilu.com>
2016-11-05 23:36:52 +0800

    init and auto model

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-// var combinations = require('combinations');
-// var myArray = ['1', '2', '3', '4'];
-//
-// console.log(combinations(myArray));
-//
 var csv2json = require('csv2json');
-// var _ = require('lodash');
+var _ = require('lodash');
 var fs = require('fs');
 var util = require('./util')();
 var json2csv = require('json2csv');
-//
-// fs.createReadStream('analyze.csv')
-//   .pipe(csv2json({
-//     // Defaults to comma.
-//     separator: ','
-//   }))
-//   .pipe(fs.createWriteStream('data.json'));
+var argv = require('yargs').argv;
 
+// mode equal raw will convert csv to json
+// mode equal analyze will analyze json data and save to csv --> analyze-result.csv
+if (argv.mode === "raw") {
+  console.log("---------------------------转化数据---------------------------");
+  fs.createReadStream('analyze.csv')
+    .pipe(csv2json({
+      // Defaults to comma.
+      separator: ','
+    }))
+    .pipe(fs.createWriteStream('data.json'));
+} else {
+  console.log("---------------------------分析数据---------------------------");
+  fs.readFile('data.json', 'utf8', function(err, data) {
+      var analyzeData = JSON.parse(data);
+      analyzeData = util.splitToArray(analyzeData);
+      var resultJson = util.analyze(analyzeData);
 
-// 转换数据
-fs.readFile('data.json', 'utf8', function(err, data) {
-    var analyzeData = JSON.parse(data);
-    analyzeData = util.splitToArray(analyzeData);
-    var resultJson = util.analyze(analyzeData);
+      // json转换为csv格式
+      var fields = ['item' , 'itemSupport','frontItem','frontItemSupport','endItem','endItemSupport','calcSupport']
+      var csv = json2csv({
+          data: resultJson,
+          fields: fields
+      });
 
-    // json转换为csv格式
-    var fields = ['item' , 'itemSupport','frontItem','frontItemSupport','endItem','endItemSupport','calcSupport']
-    var csv = json2csv({
-        data: resultJson,
-        fields: fields
-    });
-
-    fs.writeFile('analyze-result.csv', csv, function(err) {
-        if (err) throw err;
-        console.log('analyze-result saved');
-    });
-});
+      fs.writeFile('analyze-result.csv', csv, function(err) {
+          if (err) throw err;
+          console.log('analyze-result saved');
+      });
+  });
+}
